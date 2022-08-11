b3543f8f42bc5c94abb219e589bd0284e03711c5
Memo <memosoft@ya.ru>
2022-08-11 20:56:47 +0300

    feat: prepare workspace with eslint, script for package.json

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,22 @@
 function App() {
-  return <div className="App">Hello Pavels!!</div>
+  let test: any;
+
+  const tst = (a: number) => {
+    const b = a + 2;
+    return b;
+  };
+
+  test = 5;
+  test *= 2;
+  return (
+    <div className="App">
+      Hello Pavels!!!
+      {' '}
+      {test}
+      {' '}
+      {`${tst(5)}DSf${3}`}
+    </div>
+  );
 }
 
-export default App
+export default App;
