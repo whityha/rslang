08991deb877659c29a82338d2a303657c9d22e89
Pavel <2042_2042@mail.ru>
2022-08-14 13:20:39 +0300

    feat: add footer

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -1,7 +1,36 @@
 import { FC } from 'react';
+import { Box, Button, ButtonGroup } from '@mui/material';
+import RSSChool from '../assets/rs_school_js.svg';
 
 const Footer: FC = () => (
-  <footer className="footer">Footer!</footer>
+  <Box
+    component="footer"
+    sx={{
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      padding: '0px 40px 0px 40px',
+      bgcolor: 'white',
+    }}
+    className="footer"
+  >
+    <Box component="a" href="http://rs.school/" width="100px">
+      <img alt="RSSchool" src={RSSChool} />
+    </Box>
+    <ButtonGroup
+      variant="text"
+      size="small"
+      color="inherit"
+      sx={{
+        height: '50%',
+      }}
+    >
+      <Button href="https://github.com/memos77">Ihar Halavin</Button>
+      <Button href="https://github.com/whityha">Pavel Khapaliuk</Button>
+      <Button href="https://github.com/unclekoin">Pavel Koryakin</Button>
+    </ButtonGroup>
+    <Box component="span">©2022</Box>
+  </Box>
 );
 
 export default Footer;
