ec74dd74858169d039231a55b1aa928b5830d09a
Pavel <2042_2042@mail.ru>
2022-08-20 10:07:28 +0300

    refactor: add sample for game-statistic

--- src/components/statistics.tsx/statistics-block.tsx
+++ src/components/statistics.tsx/statistics-block.tsx
@@ -2,29 +2,16 @@ import {
   Box,
   Card,
   CardContent,
-  CardMedia,
   CircularProgress,
   Grid,
-  List,
-  ListItem,
-  ListItemText,
   Typography,
 } from '@mui/material';
+import GameStatisticCard from './statistic-game';
 
 const GeneralStatistics = ({ title }: {title: string}) => {
   const value = 50;
-  const cardStyles = {
-    display: 'flex',
-    width: { xs: '100%', sm: '50%' },
-  };
-  const cardImageStyle = {
-    height: 'auto',
-    minWidth: '150px',
-    width: '40%',
-    padding: '1em',
-    borderRadius: '20px',
-  };
-
+  const savannahImage = 'https://naked-science.ru/wp-content/uploads/2022/04/1440_SS_savanna_feat-1030x580-1.jpg';
+  const sprintImage = 'https://lalafemme.ca/wp-content/uploads/2016/10/run.jpg';
   return (
     <Box>
       <Typography variant="h4" mb={5}>{title}</Typography>
@@ -83,78 +70,8 @@ const GeneralStatistics = ({ title }: {title: string}) => {
               rowGap: '1rem',
             }}
           >
-            <Card sx={cardStyles}>
-              <CardMedia
-                component="img"
-                image="https://interessno.ru/wp-content/uploads/2021/02/apr60-zbkh0-1.webp"
-                sx={cardImageStyle}
-              />
-              <CardContent sx={{
-                padding: '1em',
-              }}
-              >
-                <Box sx={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                }}
-                >
-                  <Typography variant="h5" component="p">Savannah</Typography>
-                  <List disablePadding>
-                    <ListItem disablePadding>
-                      <ListItemText>
-                        <Typography variant="body1">0 слов</Typography>
-                      </ListItemText>
-                    </ListItem>
-                    <ListItem disablePadding>
-                      <ListItemText>
-                        <Typography variant="body1">0 прогресс</Typography>
-                      </ListItemText>
-                    </ListItem>
-                    <ListItem disablePadding>
-                      <ListItemText>
-                        <Typography variant="body1">0 подряд</Typography>
-                      </ListItemText>
-                    </ListItem>
-                  </List>
-                </Box>
-              </CardContent>
-            </Card>
-            <Card sx={cardStyles}>
-              <CardMedia
-                component="img"
-                image="https://marathonec.ru/wp-content/uploads/2020/04/dlitelny-beg.jpg"
-                sx={cardImageStyle}
-              />
-              <CardContent sx={{
-                padding: '1em',
-              }}
-              >
-                <Box sx={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                }}
-                >
-                  <Typography variant="h5" component="p">Sprint</Typography>
-                  <List disablePadding>
-                    <ListItem disablePadding>
-                      <ListItemText>
-                        <Typography variant="body1">0 words</Typography>
-                      </ListItemText>
-                    </ListItem>
-                    <ListItem disablePadding>
-                      <ListItemText>
-                        <Typography variant="body1">0 accurancy</Typography>
-                      </ListItemText>
-                    </ListItem>
-                    <ListItem disablePadding>
-                      <ListItemText>
-                        <Typography variant="body1">0 in a row</Typography>
-                      </ListItemText>
-                    </ListItem>
-                  </List>
-                </Box>
-              </CardContent>
-            </Card>
+            <GameStatisticCard name="Savannah" imageLink={savannahImage} />
+            <GameStatisticCard name="Sprint" imageLink={sprintImage} />
           </Box>
         </Grid>
       </Grid>
