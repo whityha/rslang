4fb982c64941c657606926d6ae0cabc552519ab6
Memo <memosoft@ya.ru>
2022-08-24 13:55:18 +0300

    feat: stars & parrots

--- src/components/game-sprint/game-sprint.tsx
+++ src/components/game-sprint/game-sprint.tsx
@@ -3,10 +3,16 @@ import { FC, useEffect, useState } from 'react';
 import { emptyGameResult, GameProps, GameWordsResult } from '../game-common/types';
 import './style.scss';
 
+const maxStars = 3;
+const maxParrots = 4;
+const birds = ['🐥', '🐦', '🐧', '🐤'];
+
 const GameSprint: FC<GameProps> = ({ words, onFinish }) => {
   const [gameResult, setGameResult] = useState<GameWordsResult>(emptyGameResult);
   const [currentWord, setCurrentWord] = useState(0);
   const [score, setScore] = useState(0);
+  const [parrots, setParrots] = useState(0);
+  const [stars, setStars] = useState(0);
   const [currentTranslate, setCurrentTranslate] = useState(0);
 
   function getNextWord() {
@@ -23,6 +29,12 @@ const GameSprint: FC<GameProps> = ({ words, onFinish }) => {
     const target = success ? gameResult.goodWords : gameResult.badWords;
     if (success) {
       setScore(score + 1);
+      if (stars === maxStars) {
+        setStars(0);
+        if (parrots < maxParrots) setParrots(parrots + 1);
+      } else {
+        setStars(stars + 1);
+      }
     }
     target.push(words[currentWord]);
 
@@ -35,6 +47,23 @@ const GameSprint: FC<GameProps> = ({ words, onFinish }) => {
     getNextWord();
   }, []);
 
+  function star() {
+    return <span className="emoji">⭐</span>;
+  }
+
+  function grayStar() {
+    return <span className="emoji no-achieved">⭐</span>;
+  }
+
+  function parrot(n: number) {
+    const addClass = n >= parrots ? 'emoji no-achieved' : 'emoji';
+    return (
+      <span className={addClass}>
+        {birds[n]}
+      </span>
+    );
+  }
+
   return (
     <div className="game-sprint">
       <div className="sprint-wrapper">
@@ -43,8 +72,13 @@ const GameSprint: FC<GameProps> = ({ words, onFinish }) => {
           <div className="sprint-status__timer">{score}</div>
         </div>
         <div className="sprint-box">
-          <div className="sprint-box__stars">Stars</div>
-          <div className="sprint-box__parrots">Parrots</div>
+          <div className="sprint-box__stars">
+            {[...Array(stars)].map(() => star())}
+            {[...Array(maxStars - stars)].map(() => grayStar())}
+          </div>
+          <div className="sprint-box__parrots">
+            {[...Array(maxParrots)].map((_v, i) => parrot(i))}
+          </div>
           <div className="sprint-box__word">{words[currentWord].word}</div>
           <div className="sprint-box__translate">{words[currentTranslate].wordTranslate}</div>
           <div className="sprint-box__buttons">
