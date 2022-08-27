6d2c8918d9e2e25fb61d21c57ef80168fd70ad6c
Pavel <2042_2042@mail.ru>
2022-08-27 13:40:30 +0300

    refactor: replace block our-team

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -1,13 +1,11 @@
 import { FC } from 'react';
 
 import TestComponent from '../test-component';
-import OurTeam from '../out-team/our-team';
 
 const Main: FC = () => (
   <>
     <h1>Main Page</h1>
     <TestComponent />
-    <OurTeam />
   </>
 );
 export default Main;
