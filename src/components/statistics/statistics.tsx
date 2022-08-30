9426de26f8702424bf1f2c7a0a500f9ee52c94c7
Memo <memosoft@ya.ru>
2022-08-30 09:31:16 +0300

    feat: charts templates

--- src/components/statistics/statistics.tsx
+++ src/components/statistics/statistics.tsx
@@ -5,6 +5,8 @@ import {
 import { useEffect, useState } from 'react';
 import { getStat } from '../../inc/api';
 import { StatOptional } from '../game-common/save-game-stat';
+import ChartNewWords from './chart-new-word';
+import ChartProgress from './chart-progress';
 import GeneralStatistics from './statistics-block';
 
 const Statistics = () => {
@@ -15,16 +17,20 @@ const Statistics = () => {
   }, []);
 
   return (
-    <Box>
-      {Object.keys(stat).length === 0 ? (<Typography variant="h4" mb={5}>Статистики еще нет, сыграйте в игры</Typography>)
-        : (
-          <>
-            {Object.keys(stat).map((day) => (
-              <GeneralStatistics day={day} stat={stat[day]} />
-            ))}
-          </>
-        )}
-    </Box>
+    <>
+      <Box>
+        {Object.keys(stat).length === 0 ? (<Typography variant="h4" mb={5}>Статистики еще нет, сыграйте в игры</Typography>)
+          : (
+            <>
+              {Object.keys(stat).map((day) => (
+                <GeneralStatistics day={day} stat={stat[day]} />
+              ))}
+            </>
+          )}
+      </Box>
+      <ChartNewWords />
+      <ChartProgress />
+    </>
   );
 };
 
