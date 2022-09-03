600751c3e6cc3e3b21a1d703655eb4c84b81db5a
Memo <memosoft@ya.ru>
2022-09-03 14:29:52 +0300

    feat: hide games for solved

--- src/components/textbook/textbook.tsx
+++ src/components/textbook/textbook.tsx
@@ -3,13 +3,17 @@ import { Box } from '@mui/material';
 import WordList from '../word-list/word-list';
 import BookCatalog from '../book-catalog/book-catalog';
 import GameCatalog from '../game-catalog/game-catalog';
+import { useWords } from '../../redux/hooks';
 
-const TextBook: FC = () => (
-  <Box sx={{ position: 'relative' }}>
-    <BookCatalog />
-    <GameCatalog />
-    <WordList />
-  </Box>
-);
+const TextBook: FC = () => {
+  const words = useWords();
+  return (
+    <Box sx={{ position: 'relative' }}>
+      <BookCatalog />
+      {words.allHard || <GameCatalog />}
+      <WordList />
+    </Box>
+  );
+};
 
 export default TextBook;
