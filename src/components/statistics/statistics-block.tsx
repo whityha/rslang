1198ddea5dea423324d20f7113aa12b79b93e0af
Memo <memosoft@ya.ru>
2022-09-03 18:47:02 +0300

    fix: stat grid

--- src/components/statistics/statistics-block.tsx
+++ src/components/statistics/statistics-block.tsx
@@ -17,49 +17,42 @@ type Props = {
 const getName = (name: string) => gamesCardData.find(({ path }) => path === name)?.title ?? 'Игра';
 
 const GeneralStatistics: FC<Props> = ({ day, stat }) => (
+
   <Box>
+    <br />
     <Typography variant="h6">
       Статистика за:
       {' '}
       {day}
     </Typography>
-    <Grid
-      container
-      spacing={2}
-    >
-      <Grid item>
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: { xs: 'column', md: 'row' },
-            columnGap: '1rem',
-            rowGap: '1rem',
-            mb: 2,
-          }}
-        >
-          <GameStatisticCard
-            name="Всего по словам"
-            imageLink="/img/words.jpg"
-            newWords={stat.new}
-            studiedWords={stat.good}
-            percent={Math.round((stat.good / (stat.good + stat.bad)) * 100)}
-          />
-          {
+
+    <Grid container spacing={2}>
+      <Grid item xl={4} md={6} xs={12}>
+        <GameStatisticCard
+          name="Всего по словам"
+          imageLink="/img/words.jpg"
+          newWords={stat.new}
+          studiedWords={stat.good}
+          percent={Math.round((stat.good / (stat.good + stat.bad)) * 100)}
+        />
+      </Grid>
+      {
                       Object.keys(stat.games).map((game) => (
-                        <GameStatisticCard
-                          name={getName(game)}
-                          imageLink={`/img/${game}.jpg`}
-                          newWords={stat.games[game].new}
-                          maxSerie={stat.games[game].serie}
-                          percent={Math.round((stat.good / (stat.good + stat.bad)) * 100)}
-                        />
+                        <Grid item xl={4} md={6} xs={12}>
+                          <GameStatisticCard
+                            name={getName(game)}
+                            imageLink={`/img/${game}.jpg`}
+                            newWords={stat.games[game].new}
+                            maxSerie={stat.games[game].serie}
+                            percent={Math.round((stat.good / (stat.good + stat.bad)) * 100)}
+                          />
+
+                        </Grid>
                       ))
 
           }
-
-        </Box>
-      </Grid>
     </Grid>
+
   </Box>
 );
 
